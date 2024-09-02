4ee50627b2d2e237914b9fa3724129f3e9e9ca83
Gabriel <suppershock777@gmail.com>
2024-09-02 19:29:01 -0300

    Feat: Criado nosso create user

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,10 +1,21 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function create(Request $request){
+        $new_post = [
+            'tilte' => 'Meu Primeiro Post',
+            'content' => 'Conteudo Qualquer',
+            'author' => 'Gabriel'
+        ];
+
+        // instanciando um model
+        $post = new Post($new_post);
+        $post->save();
+
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable = [
-        'title','description','author'
+        'tilte','content','author'
     ];
     use HasFactory;
 }
